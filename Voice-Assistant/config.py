9cf1c9b6df7ae9a50df834bbd5a11673ad91315e
KhushRawat-4 <khushctae@gmail.com>
2026-07-07 11:01:11 +0530

    Remove hardcoded API key

--- Voice-Assistant/config.py
+++ Voice-Assistant/config.py
@@ -4,6 +4,6 @@ Configuration settings fir the voice assistant.
 
 ASSISTANT_NAME = "Alice"
 
-WEATHER_API_KEY = "0b71d6dd2b8ed9af5be46d760118e637"
+WEATHER_API_KEY = "YOUR_API_KEY"
 
 DEFAULT_CITY = "Udaipur"
